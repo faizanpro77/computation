56b6fdb7bb6c790cabf7267151cc8ec38ea5c1a4
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-06 22:45:22 +0530

    [faizan] modified sort.sh

--- sort.sh
+++ sort.sh
@@ -15,3 +15,15 @@ echo $result3
 
 result4=$((a%b+c))
 echo $result4
+
+
+declare -a array
+        array[0]=${dict[$result1]}
+        array[1]=${dict[$result2]}
+        array[2]=${dict[$result3]}
+        array[3]=${dict[$result4]}
+
+echo ${array[@]}
+
+
+
